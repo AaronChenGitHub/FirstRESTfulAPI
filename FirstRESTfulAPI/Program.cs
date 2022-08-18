c652ab7452d09ecf249bfda6a5a15a16fb24855d
AaronChen <zu00731135@gmail.com>
2022-08-18 23:42:28 +0800

    new Project "MyWeb" and new EmployeeController and new Employee Index, Add Vue.js from cdn, Download Axios from GitHub to wwwroot ,Add CORS Section in appsettings.json, Setting CORS from API

--- FirstRESTfulAPI/Program.cs
+++ FirstRESTfulAPI/Program.cs
@@ -6,13 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 ConfigurationManager manager = builder.Configuration;
 builder.Services.AddDbContext<EmployeesContext>((builder) => {
     builder.UseSqlServer(manager.GetConnectionString("Employee"));
 });
+
+//CORS
+string[] corsURL = manager.GetSection("CORS").Get<string[]>();
+Console.WriteLine(corsURL[0]);
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("myweb", bul =>
+    {
+        bul.WithHeaders(corsURL[0]);
+        bul.WithMethods(corsURL[0]);
+        bul.WithOrigins(corsURL[0]);
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -26,6 +39,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("myweb");
 
 app.MapControllers();
 
