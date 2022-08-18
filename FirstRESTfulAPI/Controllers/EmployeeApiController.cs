4be985578f815958532108adb435da01dfc36720
AaronChen <zu00731135@gmail.com>
2022-08-18 22:39:45 +0800

    Use Database First From local host SqlServer, Add DbContext, Design Api to GetALL & GetOnce

---
+++ FirstRESTfulAPI/Controllers/EmployeeApiController.cs
@@ -0,0 +1,49 @@
+﻿using FirstRESTfulAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstRESTfulAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeApiController : ControllerBase
+    {
+        private readonly EmployeesContext _employeesContext;
+        public EmployeeApiController(EmployeesContext employeesContext) 
+        { 
+            _employeesContext = employeesContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll/rawdata")]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return _employeesContext.Employees.ToList();
+        }
+
+
+        [HttpGet]
+        [Route("GetOnce/{id}/rawdata")]
+        public ActionResult<IEnumerable<Employee>> Get([FromRoute(Name ="id")]int id)
+        {
+            return _employeesContext.Employees.Where(x=>x.EmployeeId == id).ToList();
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FirstRESTfulAPI/Models/EmployeesContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FirstRESTfulAPI.Models
+{
+    public partial class EmployeesContext : DbContext
+    {
+        public EmployeesContext()
+        {
+        }
+
+        public EmployeesContext(DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("Employee"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.FirstName).HasMaxLength(30);
+
+                entity.Property(e => e.LastName).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
