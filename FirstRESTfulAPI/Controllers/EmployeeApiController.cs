5555c64304d8a750ea64bab928a2ce93893e46fc
AaronChen <zu00731135@gmail.com>
2022-08-23 01:48:06 +0800

    DI IOC AddScoped & AddSingleton

--- FirstRESTfulAPI/Controllers/EmployeeApiController.cs
+++ FirstRESTfulAPI/Controllers/EmployeeApiController.cs
@@ -1,4 +1,5 @@
-﻿using FirstRESTfulAPI.Models;
+﻿using FirstRESTfulAPI.Interface;
+using FirstRESTfulAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,17 +9,18 @@ namespace FirstRESTfulAPI.Controllers
     [ApiController]
     public class EmployeeApiController : ControllerBase
     {
-        private readonly EmployeesContext _employeesContext;
-        public EmployeeApiController(EmployeesContext employeesContext) 
+        
+        private readonly IEmoloyee _employeeServices;
+        public EmployeeApiController(IEmoloyee employeeServices) 
         { 
-            _employeesContext = employeesContext;
+            _employeeServices = employeeServices;
         }
 
         [HttpGet]
         [Route("GetAll/rawdata")]
-        public ActionResult<IEnumerable<Employee>> Get()
+        public ActionResult<IEnumerable<Employee>> GetAll()
         {
-            return _employeesContext.Employees.ToList();
+            return _employeeServices.GetAll();
         }
 
 
@@ -26,7 +28,7 @@ namespace FirstRESTfulAPI.Controllers
         [Route("GetOnce/{id}/rawdata")]
         public ActionResult<IEnumerable<Employee>> Get([FromRoute(Name ="id")]int id)
         {
-            return _employeesContext.Employees.Where(x=>x.EmployeeId == id).ToList();
+            return _employeeServices.Get(id);
         }
 
 
@@ -34,25 +36,7 @@ namespace FirstRESTfulAPI.Controllers
         [Route("Add/rawdata")]
         public Message AddEmployee([FromBody] Employee employee)
         {
-            Message msg = new Message();
-            _employeesContext.Employees.Add(employee);
-            try
-            {
-                int num = _employeesContext.SaveChanges();
-                if(num > 0)
-                {
-                    msg.Code = 200;
-                    msg.msg = $"員工{employee.FirstName}新增成功!!";
-                }
-            }
-            catch(DbUpdateException ex)
-            {
-                HttpResponse response = this.Response;
-                response.StatusCode = 400;
-                msg.Code = 400;
-                msg.msg = $"員工{employee.FirstName}新增失敗!!";
-            }
-            return msg;
+            return _employeeServices.AddEmployee(employee);
         }
 
 
@@ -60,25 +44,7 @@ namespace FirstRESTfulAPI.Controllers
         [Route("update/rawdata")]
         public Message UpdateEmployee([FromBody]Employee employee)
         {
-            Message msg = new Message();
-            _employeesContext.Add(employee);
-            _employeesContext.Entry<Employee>(employee).State = EntityState.Modified;
-            try
-            {
-                int num =_employeesContext.SaveChanges();
-                if(num > 0)
-                {
-                    msg.Code = 200;
-                    msg.msg = $"員工{employee.FirstName}修改完成";
-                }
-            }
-            catch(DbUpdateException ex)
-            {
-                msg.Code = 400;
-                msg.msg = $"員工{employee.FirstName}修改失敗";
-                this.Response.StatusCode = 400;
-            }
-            return msg;
+           return _employeeServices.UpdateEmployee(employee);
         }
 
 
@@ -86,33 +52,7 @@ namespace FirstRESTfulAPI.Controllers
         [Route("delete/rawdata")]
         public Message Delete([FromQuery(Name ="id")] int id)
         {
-            Message msg = new Message();
-            var query = _employeesContext.Employees.Where(x => x.EmployeeId == id).FirstOrDefault<Employee>();
-            if (query == null)
-            {
-                msg.Code = 400;
-                msg.msg = $"查無此員工";
-            }
-            else 
-            {
-                _employeesContext.Entry<Employee>(query).State = EntityState.Deleted;
-                try
-                {
-                    int num = _employeesContext.SaveChanges();
-                    if(num>0)
-                    {
-                        msg.Code = 200;
-                        msg.msg = $"刪除成功";
-                    }
-                    
-                }
-                catch(DbUpdateException ex)
-                {
-                    msg.Code = 400;
-                    msg.msg = "刪除失敗";
-                }
-            }
-            return msg;
+            return _employeeServices.Delete(id);
         }
     }
 }
--- FirstRESTfulAPI/Program.cs
+++ FirstRESTfulAPI/Program.cs
@@ -1,3 +1,5 @@
+using FirstRESTfulAPI;
+using FirstRESTfulAPI.Interface;
 using FirstRESTfulAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,11 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-ConfigurationManager manager = builder.Configuration;
-builder.Services.AddDbContext<EmployeesContext>((builder) => {
-    builder.UseSqlServer(manager.GetConnectionString("Employee"));
-});
 
+ConfigurationManager manager = builder.Configuration;
 //CORS
 string[] corsURL = manager.GetSection("CORS").Get<string[]>();
 Console.WriteLine(corsURL[0]);
@@ -25,11 +24,17 @@ builder.Services.AddCors(opt => {
         bul.AllowAnyOrigin();
     });
 });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+builder.Services.AddDbContext<EmployeesContext>((builder) => {
+    builder.UseSqlServer(manager.GetConnectionString("Employee"));
+});
+builder.Services.AddScoped<IEmoloyee, EmployeeServices>();
+builder.Services.AddSingleton<Message>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
